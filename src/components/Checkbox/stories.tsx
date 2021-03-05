fdf0b656230a3a1b5c07b03d0a23e75fe904cd64
João Paulo Vilar <vilarjp@hotmail.com>
2021-03-04 22:10:49 -0300

    add Checkbox component

---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,47 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Checkbox, { CheckboxProps } from '.';
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+  argTypes: {
+    onCheck: { action: 'checked' },
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+        {...args}
+      />
+    </div>
+  </>
+);
